e37861b2ca802aaaa53a54edc89dbbd53a8c4a7b
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-11 18:50:05 +0900

    Title: 함수를 이용한 369 게임, Time: 292ms, Memory: 25MB, Status: Passed - CodeTree

--- 240611/함수를 이용한 369 게임/369-games-using-functions.py
+++ 240611/함수를 이용한 369 게임/369-games-using-functions.py
@@ -2,14 +2,19 @@ a, b = map(int, input().split())
 
 check = ["3", "6", "9"]
 def check_369(n):
-    return str(n)[0] in check or str(n)[1] in check
+    n_len = len(n)
+    for j in range(n_len):
+        if n[j] in check:
+            return True
+            
+    return False
 
 def can_div(n):
     return (n % 3 == 0)
 
 cnt = 0 
 for i in range(a, b + 1):
-    if check_369(i) or can_div(i):
+    if check_369(str(i)) or can_div(i):
         cnt += 1
 
 print(cnt)
