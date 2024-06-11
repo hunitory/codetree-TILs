66ad1519a0d75ba8dcdc54116cbf46ee431206a7
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-11 18:18:55 +0900

    Title: 짝수이면서 합이 5의 배수인 수, Time: 144ms, Memory: 24MB, Status: Passed - CodeTree

--- 240611/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
+++ 240611/짝수이면서 합이 5의 배수인 수/an-even-number-with-a-multiple-of-5-in-the-sum.py
@@ -1,10 +1,6 @@
 n = input()
 
-if int(n) % 2 == 0:
-    if (int(n[1]) + int(n[0])) % 5 == 0:
-        print('Yes')
-    else:
-        print("No")
-
+if int(n) % 2 == 0 and (int(n[1]) + int(n[0])) % 5 == 0:
+    print('Yes')
 else:
     print("No")
