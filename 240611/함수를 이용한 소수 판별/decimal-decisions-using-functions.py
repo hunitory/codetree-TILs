142fe9aa0a632d8c6e4f0126b12afc18d6a99059
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-11 19:56:57 +0900

    Title: 함수를 이용한 소수 판별, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240611/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 240611/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -4,6 +4,8 @@ a, b = map(int, input().split())
 
 ans = 0
 for i in range(a, b + 1):
+    if i == 1:
+        continue
     cnt = 0
     for j in range(2, int(math.sqrt(i)) + 1):
         if i % j == 0:
