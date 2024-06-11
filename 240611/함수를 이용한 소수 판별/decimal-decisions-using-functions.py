25c5e21b8fee7a522e82f9f26e8c4bb2769730f3
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-11 19:44:54 +0900

    Title: 함수를 이용한 소수 판별, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240611/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -0,0 +1,16 @@
+import math
+
+a, b = map(int, input().split())
+
+ans = 0
+for i in range(a, b + 1):
+    cnt = 0
+    for j in range(2, int(math.sqrt(i)) + 1):
+        if i % j == 0:
+           cnt += 1
+
+    if cnt == 0 :
+         ans += i
+        
+
+print(ans)
