bbc1e9a0d938755f635ffabca064545a8055f5ea
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-12 12:24:04 +0900

    Title: 사칙연산 함수, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240612/사칙연산 함수/quadratic-operations-function.py
+++ 240612/사칙연산 함수/quadratic-operations-function.py
@@ -1,10 +1,10 @@
 a, o, c = map(str, input().split())
 
 if o == '/':
-    print(int(int(a) / int(c)))
+    print(f'{int(a)} / {int(c)} = int(int(a) / int(c))')
 elif o == '+':
     print(f'{int(a)} + {int(c)} = {int(a) + int(c)}')
 elif o == '-':
-    print(int(a) - int(c))
+    print(f'{int(a)} - {int(c)} = {int(a) - int(c)}')
 elif o == '*':
     print(f'{int(a)} * {int(c)} = {int(a) * int(c)}')
