ba5f20ce9446833a268eed55802b797fbbcea23c
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-12 12:27:54 +0900

    Title: 사칙연산 함수, Time: 107ms, Memory: 24MB, Status: Passed - CodeTree

--- 240612/사칙연산 함수/quadratic-operations-function.py
+++ 240612/사칙연산 함수/quadratic-operations-function.py
@@ -1,7 +1,7 @@
 a, o, c = map(str, input().split())
 
 if o == '/':
-    print(f'{int(a)} / {int(c)} = int(int(a) / int(c))')
+    print(f'{int(a)} / {int(c)} = {int(int(a) / int(c))}')
 elif o == '+':
     print(f'{int(a)} + {int(c)} = {int(a) + int(c)}')
 elif o == '-':
