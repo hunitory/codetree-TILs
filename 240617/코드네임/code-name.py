477a6283f68c1bfc33fa73ec40757ec063e1172f
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-17 05:38:41 +0900

    Title: 코드네임, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/코드네임/code-name.py
+++ 240617/코드네임/code-name.py
@@ -1,12 +1,23 @@
-class student:
-    def __init__(self, codeName, score):
-        self.codeName = codeName
+# 클래스 선언
+class User:
+    def __init__(self, code_name, score):
+        self.code_name = code_name
         self.score = score
-        
-students = []
-for i in range(5):
-    input_info = input().split()
-    students.append(student(input_info[0], int(input_info[1])))
 
-ans = min(students, key=lambda x:x.score)
-print(ans.codeName, ans.score)
+
+# 변수 선언 및 입력
+users = []
+
+for _ in range(5):
+    code_name, score = tuple(input().split())
+    users.append(User(code_name, int(score)))
+
+
+# 최소 점수를 갖는 유저 찾기
+min_idx = 0
+for i in range(1, 5):
+    if users[min_idx].score > users[i].score:
+        min_idx = i
+
+# 출력
+print(users[min_idx].code_name, users[min_idx].score)
