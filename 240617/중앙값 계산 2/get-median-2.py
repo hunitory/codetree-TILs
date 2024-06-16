d19f97e2fa5e704ea02b30b83e016f7a0c999b0a
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-17 04:58:42 +0900

    Title: 중앙값 계산 2, Time: 108ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/중앙값 계산 2/get-median-2.py
+++ 240617/중앙값 계산 2/get-median-2.py
@@ -5,5 +5,6 @@ num_list = list(map(int, input().split()))
 ans_list = []
 for i in range(n):
     ans_list.append(num_list[i])
+    ans_list.sort()
     if i % 2 == 0:
         print(ans_list[i // 2], end=" ")
