0786c3c7983eeb9c970450318a348212341524ec
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-17 06:02:57 +0900

    Title: 상품 코드, Time: 135ms, Memory: 24MB, Status: Passed - CodeTree

--- 240617/상품 코드/product-code.py
+++ 240617/상품 코드/product-code.py
@@ -1,10 +1,10 @@
 class product:
-    def __init__(self, productName="codetree", productCode=50):
+    def __init__(self, productName, productCode):
         self.productName = productName
         self.productCode = productCode
     
 
-product1 = product()
+product1 = product("codetree", 50)
 print(f'product {product1.productCode} is {product1.productName}')
 productName, productCode = input().split()
 product1 = product(productName, productCode)
