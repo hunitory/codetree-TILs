4082e3aa084587885f2fad2f03f3ad35385b80ad
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-18 22:30:15 +0900

    Title: 국영수 순이지, Time: 238ms, Memory: 24MB, Status: Passed - CodeTree

--- 240618/국영수 순이지/korean-english-math-order.py
+++ 240618/국영수 순이지/korean-english-math-order.py
@@ -1,12 +1,22 @@
+# 클래스 선언
+class Student:
+    def __init__(self, name, korean, english, math):
+        self.name = name
+        self.korean = korean
+        self.english = english
+        self.math = math
+
+
+# 변수 선언 및 입력
 n = int(input())
+students = []
+for _ in range(n):
+    name, korean, english, math = tuple(input().split())
+    students.append(Student(name, int(korean), int(english), int(math)))
 
-students= []
+# Custom Comparator를 활용한 정렬
+students.sort(key = lambda x: (-x.korean, -x.english, -x.math))
 
-for i in range(n):
-    name, kor, eng, math = map(str, input().split())
-    students.append([name, int(kor), int(eng), int(math)])
-
-students.sort(key=lambda x:(-x[1], -x[2], -x[3]))
-
+# 출력
 for student in students:
-    print(*student)
+    print(student.name, student.korean, student.english, student.math)
