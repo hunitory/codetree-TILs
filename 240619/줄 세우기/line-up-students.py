feb5cc163a7b211c29c46fa8f60e60439a595a89
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-19 21:34:17 +0900

    Title: 줄 세우기, Time: 155ms, Memory: 25MB, Status: Passed - CodeTree

--- 240619/줄 세우기/line-up-students.py
+++ 240619/줄 세우기/line-up-students.py
@@ -6,7 +6,7 @@ for i in range(1, n + 1):
     high, weight = map(int, input().split())
     students.append((high, weight, number))
 
-students.sort(key=lambda x:(-x[0], -x[1], -x[2]))
+students.sort(key=lambda x:(-x[0], -x[1], x[2]))
 
 for student in students:
     print(*student)
