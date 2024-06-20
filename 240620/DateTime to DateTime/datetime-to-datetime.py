fac981a3e0d6381bf70a9ceaece4029a9de2157e
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-20 17:50:36 +0900

    Title: DateTime to DateTime, Time: 139ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/DateTime to DateTime/datetime-to-datetime.py
@@ -0,0 +1,25 @@
+a, b, c = map(int, input().split())
+
+if a <= 11 and b <= 11 and c < 11:
+    print(-1)
+
+day, hour, minute = 0, 0, 0
+
+if c < 11:
+    hour -= 1
+    minute = 60 - (11 - c)
+else:
+    minute = c - 11
+
+if b < 11:
+    day -= 1
+    hour = 12 - (11 - b)
+else:
+    hour = b - 11
+
+if a == 11:
+    day += 1
+else:
+    day = a - 11
+
+print(day * 1440 + hour * 60 + minute  )
