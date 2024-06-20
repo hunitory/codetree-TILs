3d022b0fdf26b896fc78dcd166b0c8726087ef9a
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-20 17:27:30 +0900

    Title: Date to Date, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240620/Date to Date/date-to-date.py
@@ -0,0 +1,16 @@
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+m1, d1, m2, d2 = map(int, input().split())
+
+ans = 0
+
+month, day = 0, 0 
+
+month = m2 - m1
+if month > 2:
+    ans += sum(num_of_days[m1 + 1: m2])
+
+ans += num_of_days[m1] - d1 + 1
+ans += d2
+
+print(ans)
