6c91587c2952d4474c5e88eb4e0de6a9ba3dcb18
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-20 18:16:28 +0900

    Title: DateTime to DateTime, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

--- 240620/DateTime to DateTime/datetime-to-datetime.py
+++ 240620/DateTime to DateTime/datetime-to-datetime.py
@@ -1,29 +1,11 @@
+# 변수 선언 및 입력
 a, b, c = map(int, input().split())
 
-if a <= 11 and b <= 11 and c < 11:
+# 차이를 계산합니다.
+diff = (a * 1440 + b * 60 + c) - (11 * 1440 + 11 * 60 + 11)
+
+# 출력
+if diff < 0:
     print(-1)
-
-day, hour, minute = 0, 0, 0
-
-if c < 11:
-    hour -= 1
-    minute = 60 - (11 - c)
 else:
-    minute = c - 11
-
-if b < 11:
-    day -= 1
-    hour += 12 - (11 - b)
-else:
-    hour += b - 11
-
-if a > 11:
-    day += a - 11 + 1
-
-print(day, hour, minute)
-
-
-if day > 1:
-    print(day * 1440 + hour * 60 + minute - (11 * 60 + 10 + 50))
-else:
-    print(day * 1440 + hour * 60 + minute)
+    print(diff)
