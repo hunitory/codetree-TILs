732b9934fe6d064929302e70c331fd77eef58a06
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-20 18:46:39 +0900

    Title: 그 요일은, Time: 140ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240620/그 요일은/the-day-of-the-day.py
+++ 240620/그 요일은/the-day-of-the-day.py
@@ -2,8 +2,9 @@ num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
 
 m1, d1, m2, d2 = map(int, input().split())
-target_day = input()
 
+target_day = input()
+target_day_index = days.index(target_day)
 
 standard_day = sum(num_of_days[:m1]) + d1
 
@@ -11,4 +12,10 @@ problem_day = sum(num_of_days[:m2]) + d2
 
 diff_day = problem_day - standard_day
 
-print(diff_day // 7 + 1)
+ans = 0
+ans += diff_day // 7
+
+if target_day_index >= diff_day - (ans * 7) + 1:
+    ans += 1
+
+print(ans)
