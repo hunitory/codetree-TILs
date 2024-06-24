83c100e7c5e14691e34a7d34502b67d0e931d77f
hunitory <139518173+hunitory@users.noreply.github.com>
2024-06-24 14:14:30 +0900

    Title: 여러가지 진수변환, Time: 122ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240624/여러가지 진수변환/various-numeral-system-transformations.py
@@ -0,0 +1,13 @@
+N, B = map(int, input().split())
+
+def solution(n, q):
+    rev_base = ''
+
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+
+    return rev_base[::-1] 
+    # 역순인 진수를 뒤집어 줘야 원래 변환 하고자하는 base가 출력
+    
+print(solution(N, B))
