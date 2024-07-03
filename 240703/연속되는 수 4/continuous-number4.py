0d14b8e009c27f1c6323486d2e69cae77f390b67
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-03 17:25:10 +0900

    Title: 연속되는 수 4, Time: 116ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240703/연속되는 수 4/continuous-number4.py
+++ 240703/연속되는 수 4/continuous-number4.py
@@ -3,11 +3,11 @@ n = int(input())
 num_list = [int(input()) for _ in range(n)]
 
 ans, cnt = 0 , 0
-for i in range(n):
-    if i >= 1 and num_list[i] < num_list[i - 1]:
-        cnt = 0
-    else:
+for i in range(n - 1):
+    if num_list[i] < num_list[i + 1]:
         cnt += 1
+    else:
+        cnt = 1
     
     ans = max(ans, cnt)
 
