318b586af13e37d8189c8e5a1bcb339a79649a87
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-03 17:13:37 +0900

    Title: 연속되는 수 3, Time: 100ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240703/연속되는 수 3/continuous-number3.py
+++ 240703/연속되는 수 3/continuous-number3.py
@@ -3,9 +3,9 @@ n = int(input())
 num_list = [int(input()) for _ in range(n)]
 
 ans, cnt = 0 , 1
-for i in range(1, n):
+for i in range(n):
     # 곱해서 음수 -> 부호가 다르다는 것
-    if num_list[i] * num_list[i - 1] < 0:
+    if i >= 1 and num_list[i] * num_list[i - 1] < 0:
         cnt = 1
     else:
         cnt += 1
