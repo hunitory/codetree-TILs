1517ab788b2a5d1627a91ac4a58265b87d50683a
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-03 16:48:30 +0900

    Title: 연속되는 수 2, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240703/연속되는 수 2/continuous-number2.py
+++ 240703/연속되는 수 2/continuous-number2.py
@@ -6,19 +6,15 @@ for i in range(n):
     num = int(input())
     num_list.append(num)
 
-ans = 1
+ans = 0
 cnt = 0
 
 for i in range(n - 1):
     if num_list[i] == num_list[i + 1]:
         cnt += 1
     else:
-        if ans < cnt + 1:
-            ans = cnt + 1
+        if ans < cnt:
+            ans = cnt
             cnt = 0
 
-if ans < cnt + 1:
-    ans = cnt + 1
-    cnt = 0
-    
-print(ans)
+print(ans + 1)
