cc71e78ec894d56847702203f7ba57be0f7ca5bf
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-03 15:57:14 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 252ms, Memory: 55MB, Status: Passed - CodeTree

--- 240703/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240703/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -10,44 +10,45 @@ for i in range(1, 3):
     if i == 1:
         mx1, my1, mx2, my2 = x1, y1, x2, y2
 
-    for y in range(y1 + offset, y2 + offset + 1):
-        for x in range(x1 + offset, x2 + offset + 1):
-            arr[y][x] += i
+    for y in range(y1 + offset, y2 + offset):
+        for x in range(x1 + offset, x2 + offset):
+            arr[y][x] = i
 
 ax, ay = 0, 0
 
-for i in range(my1 + offset, my2 + offset + 1):
+for ii in range(my1 + offset, my2 + offset):
     x = 0
     dx = 0
-    for j in range(mx1 + offset, mx2 + offset + 1):
-        if arr[i][j] == 1:
+    for jj in range(mx1 + offset, mx2 + offset):
+        if arr[ii][jj] == 1:
             x += 1
-            if dx > 0 :
+            if dx > 0:
                 x += dx
                 dx = 0
         else:
-            dx += 1
+            if x > 0:
+                dx += 1
 
     if x > ax:
         ax = x
 
-for i in range(mx1 + offset, mx2 + offset + 1):
+for iii in range(mx1 + offset, mx2 + offset + 1):
     y = 0
     dy = 0
-    for j in range(my1 + offset, my2 + offset + 1):
-        if arr[j][i] == 1:
+    for jjj in range(my1 + offset, my2 + offset + 1):
+        if arr[jjj][iii] == 1:
             y += 1
-            if dy > 0 :
+            if dy > 0:
                 y += dy
                 dy = 0
         else:
-            dy += 1
+            if y > 0:
+                dy += 1
 
     if y > ay:
         ay = y
 
-
 if ax == 0 or ay == 0:
     print(0)
 else:
-    print((ax - 1) * (ay - 1))
+    print((ax) * (ay))
