aece006f97d0202750ef4daabb2ea223249c3529
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-03 15:00:37 +0900

    Title: 잔해물을 덮기 위한 사각형의 최소 넓이, Time: 134ms, Memory: 54MB, Status: Wrong Answer - CodeTree

--- 240703/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
+++ 240703/잔해물을 덮기 위한 사각형의 최소 넓이/minimum-area-of-rectangle-to-cover-debris.py
@@ -12,24 +12,36 @@ for i in range(1, 3):
 
     for y in range(y1 + offset, y2 + offset + 1):
         for x in range(x1 + offset, x2 + offset + 1):
-            arr[y][x] = i
+            arr[y][x] += i
 
 ax, ay = 0, 0
 
 for i in range(my1 + offset, my2 + offset + 1):
     x = 0
+    dx = 0
     for j in range(mx1 + offset, mx2 + offset + 1):
         if arr[i][j] == 1:
             x += 1
+            if dx > 0 :
+                x += dx
+                dx = 0
+        else:
+            dx += 1
 
     if x > ax:
         ax = x
 
 for i in range(mx1 + offset, mx2 + offset + 1):
     y = 0
+    dy = 0
     for j in range(my1 + offset, my2 + offset + 1):
         if arr[j][i] == 1:
             y += 1
+            if dy > 0 :
+                y += dy
+                dy = 0
+        else:
+            dy += 1
 
     if y > ay:
         ay = y
