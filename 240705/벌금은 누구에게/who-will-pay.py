6e2c71c30238e6008a5b7f5a2b0153134e6876de
hunitory <139518173+hunitory@users.noreply.github.com>
2024-07-05 15:20:32 +0900

    Title: 벌금은 누구에게, Time: 184ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 240705/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,10 @@
+N, M, K = map(int, input().split())  # 학생 수 , 벌금 부여 수, 벌칙 최대 
+
+students = [0] * (N + 1)
+
+for m in range(M):
+    punish = int(input())
+    students[punish] += 1
+    if students[punish] >= K:
+        print(punish)
+        break
