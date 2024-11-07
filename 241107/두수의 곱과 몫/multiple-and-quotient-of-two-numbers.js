e89039413fdcfcfc569db40f86174088102dd8b2
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 16:58:03 +0900

    Title: 두수의 곱과 몫, Time: 173ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241107/두수의 곱과 몫/multiple-and-quotient-of-two-numbers.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+console.log(`${a} * ${b} = ${a * b}`);
+console.log(`${a} / ${b} = ${a / b}`);
