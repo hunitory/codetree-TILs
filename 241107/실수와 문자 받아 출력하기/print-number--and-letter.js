b58f013f8ee6dc5d46995a18db31a86b8e6f2581
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 16:17:57 +0900

    Title: 실수와 문자 받아 출력하기, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241107/실수와 문자 받아 출력하기/print-number--and-letter.js
@@ -0,0 +1,22 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+let inputs = [];
+
+
+rl.on("line", (input) => {
+    inputs.push(input);
+}).on("close", ()=>{
+    inputs.forEach(input => {
+        if (Number(input)) {
+            input = Number(input);
+            console.log(input.toFixed(2));
+        } else {
+            console.log(input)
+        }
+    })
+})
