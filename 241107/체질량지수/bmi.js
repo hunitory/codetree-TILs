467ce423948923ebd56680be4324ac25c0d00048
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 17:46:34 +0900

    Title: 체질량지수, Time: 176ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 241107/체질량지수/bmi.js
@@ -0,0 +1,12 @@
+const fs = require("fs");
+
+let [h, w] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+const bmi = (10000 * w) / (h * h)
+
+if ( bmi >= 25 ) {
+    console.log(bmi);
+    console.log("Obesity");
+} else {
+    console.log(bmi);
+}
