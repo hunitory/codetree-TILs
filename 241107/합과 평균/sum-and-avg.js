0935ca15a451bfeb5b94dfc6750bd7b4e403257c
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 17:29:55 +0900

    Title: 합과 평균, Time: 196ms, Memory: 14MB, Status: Passed - CodeTree

--- 241107/합과 평균/sum-and-avg.js
+++ 241107/합과 평균/sum-and-avg.js
@@ -2,4 +2,4 @@ const fs = require("fs")
 
 let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
-console.log(`${a + b} ${(a + b) / 2}`);
+console.log(`${a + b} ${((a + b) / 2).toFixed(1)}`);
