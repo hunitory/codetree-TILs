7a22dde066f33d8d2459d454dbb100c696453136
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 17:32:43 +0900

    Title: 세 수의 합과 평균 구하기, Time: 184ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 241107/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -0,0 +1,15 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    ouput: process.stdout
+})
+
+let a, b, c;
+
+rl.on("line", (input) => {
+    [a, b, c] = input().trim().split(" ").map(Number);
+}).on("close", () => {
+    console.log(a + b + c);
+    console.log((a + b + c) / 3);
+})
