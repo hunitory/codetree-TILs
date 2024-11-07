7edf82cb74f7ca4a7a3eae790a187fc851108cad
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 17:59:38 +0900

    Title: 비교에 따른 연산, Time: 183ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241107/비교에 따른 연산/operation-based-on-comparison.js
@@ -0,0 +1,9 @@
+const fs = require("fs")
+
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+if ( a > b ) {
+    console.log(a * b);
+} else {
+    console.log(Math.floor(b / a));
+}
