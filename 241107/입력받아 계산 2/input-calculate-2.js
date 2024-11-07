4b587e45c736587a2751d0ef330be474224c745d
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-07 13:25:37 +0900

    Title: 입력받아 계산 2, Time: 176ms, Memory: 14MB, Status: Passed - CodeTree

---
+++ 241107/입력받아 계산 2/input-calculate-2.js
@@ -0,0 +1,12 @@
+const readline = require("readline");
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+rl.on("line", (input) => {
+    let [a, b] = input.split(" ").map(Number);
+    console.log(a * b);
+    rl.close();
+})
