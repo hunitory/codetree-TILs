b37eade99e872e02d728e48d2de4455fb89b0214
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-08 11:49:47 +0900

    Title: 알파벳에 따른 평가, Time: 189ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 241108/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 241108/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -1,8 +1,8 @@
 const readline = require("readline");
 
 const rl = readline.createInterface({
-    input = process.stdin,
-    output = process.stdout
+    input:  process.stdin,
+    output: process.stdout
 });
 
 let s;
