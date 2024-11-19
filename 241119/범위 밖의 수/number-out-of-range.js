9f6d9d533a81354745d26aac838ac51166f2784c
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-19 23:33:58 +0900

    Title: 범위 밖의 수, Time: 196ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241119/범위 밖의 수/number-out-of-range.js
@@ -0,0 +1,19 @@
+const readline = require("readline")
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+
+let a;
+
+rl.on("line", (input) => {
+    a = Number(input.trim())
+}).on("close", () => {
+    if (a < 10 || a > 20) {
+        console.log("yes");
+    } else {
+        console.log("no");
+    }
+});
