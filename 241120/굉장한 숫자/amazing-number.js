772d16dbe40bc0353d5d255b6b3a55436631c778
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 00:36:20 +0900

    Title: 굉장한 숫자, Time: 164ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241120/굉장한 숫자/amazing-number.js
@@ -0,0 +1,9 @@
+const fs = require("fs");
+
+let n = Number(fs.readFileSync(0).toString().trim())
+
+if (n % 2 != 0 && n % 3 == 0 || n % 2 == 0 && n % 5 == 0 ) {
+    console.log("true")
+} else {
+    console.log("false")
+}
