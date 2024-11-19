3ae25bd24e8e63e102bfa6e8314d2c3bb796da71
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 02:59:36 +0900

    Title: 장학금, Time: 160ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/장학금/scholarship.js
+++ 241120/장학금/scholarship.js
@@ -7,7 +7,9 @@ if (a >= 90) {
         console.log(100000)
     } else if (b >= 90) {
         console.log(50000)
-    } 
+    } else {
+        console.log(0)
+    }
 } else {
     console.log(0)
 }
