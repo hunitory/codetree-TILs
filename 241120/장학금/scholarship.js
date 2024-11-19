21f8966efb8e5d34c9b765a9b52891d1d796ebde
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 02:58:53 +0900

    Title: 장학금, Time: 158ms, Memory: 15MB, Status: Wrong Answer - CodeTree

--- 241120/장학금/scholarship.js
+++ 241120/장학금/scholarship.js
@@ -4,9 +4,9 @@ let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
 
 if (a >= 90) {
     if (b >= 95) {
-        console.log(10)
+        console.log(100000)
     } else if (b >= 90) {
-        console.log(5)
+        console.log(50000)
     } 
 } else {
     console.log(0)
