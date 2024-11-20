db9aac1f3561c02d979d996ab0b1221c4247048a
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 11:11:25 +0900

    Title: 중앙값 구하기, Time: 179ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/중앙값 구하기/find-the-median.js
+++ 241120/중앙값 구하기/find-the-median.js
@@ -3,6 +3,4 @@ const fs = require("fs")
 let numbers = fs.readFileSync(0).toString().trim().split(" ").map(Number)
 numbers.sort((a, b) => a - b)
 
-let [a, b, c] = [numbers[0], numbers[1], numbers[2]]
-
-console.log(b)
+console.log(numbers[1])
