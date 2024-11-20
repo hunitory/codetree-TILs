598ae04440478c521dbc99b6b8de0d63344b1cae
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 16:24:23 +0900

    Title: a 부터 b 까지, Time: 166ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241120/a 부터 b 까지/a-to-b.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+
+let [a, b] = fs.readFileSync(0).toString().trim().split(" ").map(Number);
+
+while (a <= b) {
+    if (a > b) {
+        break;
+    }
+    process.stdout.write(`${a} `);
+
+    if (a % 2 === 0) {
+        a += 3;
+    } else {
+        a *= 2;
+    }
+
+}
