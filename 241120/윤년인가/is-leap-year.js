5053ae8d668dc42ebd340f87fb22373601aec310
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-20 10:35:18 +0900

    Title: 윤년인가, Time: 171ms, Memory: 15MB, Status: Passed - CodeTree

--- 241120/윤년인가/is-leap-year.js
+++ 241120/윤년인가/is-leap-year.js
@@ -3,8 +3,12 @@ const fs = require("fs");
 let y = Number(fs.readFileSync(0).toString().trim());
 
 if ( y % 4 == 0) {
-    if (y % 100 == 0 && y % 400 != 0){
-        console.log("false")
+    if (y % 100 == 0 && y % 400 != 0) {
+        console.log("false");
+    } else {
+        console.log("true")
     }
-    console.log("true");
+
+} else {
+    console.log("false");
 }
