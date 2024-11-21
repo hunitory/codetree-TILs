8d27658f56a2411e65c47d0ea7831f2c3b85f86e
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-21 15:57:44 +0900

    Title: 홀수만 더하기, Time: 183ms, Memory: 15MB, Status: Passed - CodeTree

--- 241121/홀수만 더하기/add-only-odd-numbers.js
+++ 241121/홀수만 더하기/add-only-odd-numbers.js
@@ -9,8 +9,8 @@ let ans = 0;
 rl.on("line", (input) => {
     inputs.push(Number(input.trim()))
 }).on("close", () => {
-    for (let i = 1; i <= input[0]; i ++ ) {
-        if ((inputs[i] % 2 != 0) && (inputs[i] % 3 === 0)) {
+    for (let i = 1; i <= inputs[0]; i++ ) {
+        if (((inputs[i] % 2) != 0) && ((inputs[i] % 3) === 0)) {
             ans += inputs[i];
         }
     }
