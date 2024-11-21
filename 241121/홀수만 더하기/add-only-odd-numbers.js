8ff4a691cf0d580918d5758a01be9ce7eb8d8a3a
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-21 15:53:19 +0900

    Title: 홀수만 더하기, Time: 206ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241121/홀수만 더하기/add-only-odd-numbers.js
@@ -0,0 +1,18 @@
+const readline = require("readline");
+const { stdin : input, stdout : output } = require("process");
+
+const rl = readline.createInterface({ input, output });
+
+let inputs = [];
+let ans = 0;
+
+rl.on("line", (input) => {
+    inputs.push(Number(input.trim()))
+}).on("close", () => {
+    for (let i = 1; i <= input[0]; i ++ ) {
+        if ((inputs[i] % 2 != 0) && (inputs[i] % 3 === 0)) {
+            ans += inputs[i];
+        }
+    }
+    console.log(ans);
+})
