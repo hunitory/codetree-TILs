cf33a4d529a9727818388d792a5ba5068cefe207
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-26 14:01:54 +0900

    Title: 0 나올때까지 출력, Time: 204ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241126/0 나올때까지 출력/print-until-zero-is-given.js
@@ -0,0 +1,23 @@
+const readline = require("readline");
+
+const { stdin : input, stdout : output} = require("process");
+
+const rl = readline.createInterface({ input, output });
+
+let inputs = [];
+let a = 0, cnt = 0;
+
+rl.on("line", (input) => {
+    a = Number(input.trim())
+    if ( a === 0 ) {
+        rl.close();
+    }
+    inputs.push(input)
+    cnt++;
+
+}).on("close", () => {
+
+    for (let i = 0; i < inputs.length; i++ ){
+        console.log(inputs[i])
+    }
+})
