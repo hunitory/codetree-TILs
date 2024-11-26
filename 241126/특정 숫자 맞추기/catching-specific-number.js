3a0a7d78541e09c6afe437c189774ef6b0c32d46
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-26 15:04:03 +0900

    Title: 특정 숫자 맞추기, Time: 184ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241126/특정 숫자 맞추기/catching-specific-number.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+let inputs = fs.readFileSync(0).toString().trim().split("\n").map(Number);
+
+for ( let i = 0; i <= inputs.length - 1; i++ ) {
+    if ( inputs[i] > 25 ) {
+        console.log("Lower")
+    } else if (inputs[i] === 25) {
+        console.log("Good");
+    } else {
+        console.log("Higher")
+    }
+}
