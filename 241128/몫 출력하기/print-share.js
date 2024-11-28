3494300225f35b16a84848dc3d884c9616f9aa60
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-28 20:33:28 +0900

    Title: 몫 출력하기, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 241128/몫 출력하기/print-share.js
+++ 241128/몫 출력하기/print-share.js
@@ -2,27 +2,14 @@
 const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
-let index = 0;
 let cnt = 0;
-
-// 언제 끝날지 모르므로
-// 계속 반복합니다.
-while (true) {
-    // 입력을 받습니다.
+for ( let index = 0 ;cnt < 3 ; index ++) {
     let n = Number(input[index]);
-    index++;
-    
-    // n이 홀수라면 아무 작업도 하지 않고, n이 짝수라면 n/2를 출력하는 작업을 3번 한 뒤 종료합니다.
-    let result = n;
-    if (n % 2 === 0) {
-        result = parseInt(n / 2);
-        cnt++;
-    }
-    else {
-        continue;
-    }
-    console.log(result);
-    if (cnt >= 3) {
-        break;
+
+    if (n % 2 === 1) {
+        continue
+    } else {
+        console.log(parseInt(n / 2));
+        cnt++
     }
 }
