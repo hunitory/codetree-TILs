0a6559dad89ff827d8c70467928082f537f36523
hunitory <139518173+hunitory@users.noreply.github.com>
2024-11-28 19:20:06 +0900

    Title: 출석 부르기 2, Time: 190ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241128/출석 부르기 2/calling-attendance-2.js
@@ -0,0 +1,21 @@
+const readline = require("readline");
+const { stdin : input, stdout : output } = require("process");
+const rl = readline.createInterface({ input , output });
+
+let a;
+
+rl.on("line", (input) => {
+    a = Number(input.trim());
+    if (a === 1) {
+        console.log("John")
+    } else if (a === 2) {
+        console.log("Tom");
+    } else if (a === 3) {
+        console.log("Paul");
+    } else if (a === 4) {
+        console.log("Sam");
+    } else {
+        console.log("Vacancy")
+        rl.close();
+    }
+})
