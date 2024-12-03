a2fe946971d67d91c00d64e54f1337ebebcd7458
hunitory <139518173+hunitory@users.noreply.github.com>
2024-12-04 02:07:38 +0900

    Title: 조건에 따른 연산, Time: 165ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241204/조건에 따른 연산/operatino-by-rule.js
@@ -0,0 +1,18 @@
+const fs = require("fs");
+
+let n = Number(fs.readFileSync(0).toString());
+
+let cnt = 0;
+
+while ( n < 1000) {
+    if (n % 2 === 1) {
+        n *= 3
+        n++
+    } else {
+        n *= 2
+        n += 2
+    }
+    cnt++
+}
+
+console.log(cnt + 1);
