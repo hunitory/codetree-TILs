790f3063a06857272efe3a03436829a434816504
hunitory <139518173+hunitory@users.noreply.github.com>
2024-12-04 12:35:27 +0900

    Title: 정사각형 별표 출력하기, Time: 195ms, Memory: 15MB, Status: Passed - CodeTree

--- 241204/정사각형 별표 출력하기/print-stars-in-square.js
+++ 241204/정사각형 별표 출력하기/print-stars-in-square.js
@@ -5,14 +5,16 @@ const { stdin : input, stdout : output } = require("process");
 const rl = readline.createInterface({ input, output });
 
 let n = 0;
+let str = '';
 
 rl.on("line", (input) => {
     n = Number(input.trim())
 }).on("close", () => {
     for (let i = 0; i < n; i++ ) {
+        str = '';
         for (let j = 0 ; j < n; j++ ){
-            process.stdout.write('*' + "");
+            str += "*";
         }
-        console.log();
+        console.log(str);
     }
 })
