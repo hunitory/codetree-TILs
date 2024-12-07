705b6193a77f739552f9ed37f85ea1637f48dade
hunitory <139518173+hunitory@users.noreply.github.com>
2024-12-08 01:56:12 +0900

    Title: 별 그리기, Time: 199ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 241208/별 그리기/star-drawing.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for ( let i = 0; i < n; i++ ) {
+    str = "";
+    for ( let j = 1; j + i < n ; j++) {
+        str += " ";
+    }
+    
+    for ( let j = n * 2; j - 1 - (2 * i) < n * 2 ; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
+
+for ( let i = 1; i < n; i++ ) {
+    str = "";
+    for ( let j = n; j - i < n ; j++) {
+        str += " ";
+    }
+    
+    for ( let j = n ; j + 2 * i - 1 <= n * 2 ; j++) {
+        str += "*";
+    }
+    console.log(str);
+}
