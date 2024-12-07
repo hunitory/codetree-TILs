2cb643d9caa8666fec2765f238cefed09709c86b
hunitory <139518173+hunitory@users.noreply.github.com>
2024-12-08 02:23:23 +0900

    Title: 특정 규칙에 따른 문자 출력, Time: 186ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 241208/특정 규칙에 따른 문자 출력/character-output-according-to-rule.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+const n = Number(fs.readFileSync(0).toString().trim());
+
+let str = "";
+
+for ( let i = 0; i < n; i++ ) {
+    str = "";
+    for (let j = 0; j + i + 1 < n; j++) {
+        str += "  "
+    }
+
+    for (let j = n; j - 1 - i < n; j++) {
+        str += "@ "
+    }
+    console.log(str);
+}
+
+for ( let i = 1; i < n; i++ ) {
+    str = "";
+    for (let j = 0; j + i < n; j++) {
+        str += "@ "
+    }
+    console.log(str);
+}
